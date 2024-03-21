b2992afd92b1e3e9b18bda661507b4bb44cfc3b2
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-03-21 06:32:39 +0100

    Generates 10 random numbers asynchronously using list comprehension.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ a python module to returns 10 random numbers using async comprehension"""
-from typing import Generator, List
-import asyncio
-import random
+
+
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
