90d4abfdca7d8855b0d197b6860fcb209100acbc
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-03-21 05:27:57 +0100

    a coroutine called async_generator that takes no arguments.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,7 +4,14 @@ import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
+    """
+    async_generator - function to loop 10 times
+    Arguments:
+        no arguments
+    Returns:
+        nothing
+    """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
