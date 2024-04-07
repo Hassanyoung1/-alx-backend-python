68e10c4007ef6e425db9cf7f0c3297a21edd994e
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-07 13:57:17 +0100

    repo licence

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -78,3 +78,20 @@ class TestGithubOrgClient(unittest.TestCase):
                         self.assertEqual(repo, {"name": "Alx"})
                     mock_public_repos_url.assert_called_once()
                     mock_get_json.assert_called_once()
+
+            @parameterized.expand([
+                ({'license': {'key': 'my_license'}}, 'my_license', True),
+                ({'license': {'key': 'other_license'}}, 'my_license', False),
+            ])
+
+            def test_has_license(self, repo, license_key, expected):
+                """
+                Test the has_license method of GithubOrgClient class.
+                Args:
+                repo (dict): The repository to test.
+                license_key (str): The license key to test.
+                expected (bool): The expected result.
+                """
+                test = GithubOrgClient("test_org")
+                result = test.has_license(repo, license_key)
+                self.assertEqual(result, expected)
